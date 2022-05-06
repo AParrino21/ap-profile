f0476b386fdafd636097cd617b9e02a4fd216955
Anthony Parrino <anthony.parrino@yahoo.com>
2022-05-05 23:12:55 -0400

    testing three

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Canvas } from '@react-three/fiber'
+import Box from './components/Box/Box'
+
+function App() {
+  return (
+    <div className="App">
+      <Canvas>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <Box position={[-1.2, 0, 0]} />
+        <Box position={[1.2, 0, 0]} />
+      </Canvas>
+    </div>
+  );
+}
+
+export default App;

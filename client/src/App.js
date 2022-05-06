f947ffa8cfd2b6df8a60814a27e380e6fcdac19d
Anthony Parrino <anthony21parrino21@gmail.com>
2022-05-05 23:54:17 -0400

    rendering an interactive cube

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import { Canvas } from '@react-three/fiber'
-import Box from './components/Box/Box'
+import { OrbitControls } from '@react-three/drei'
+import Box from './components/Box/Box';
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <Canvas>
-        <ambientLight />
-        <pointLight position={[10, 10, 10]} />
-        <Box position={[-1.2, 0, 0]} />
-        <Box position={[1.2, 0, 0]} />
+      <Canvas className='canvas'>
+        <OrbitControls enableZoom={false}/>
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[-2, 5, 2]} intensity={1} />
+        <Box />
       </Canvas>
     </div>
   );
--- client/src/components/Box/Box.js
+++ client/src/components/Box/Box.js
@@ -1,27 +1,17 @@
-import React, { useRef, useState } from 'react'
-import ReactDOM from 'react-dom'
-import { Canvas, useFrame } from '@react-three/fiber'
+import React from 'react'
+import { useLoader } from '@react-three/fiber'
+import { TextureLoader } from 'three/src/loaders/TextureLoader'
+import Ice from './ice.jpg'
 
-const Box = (props) => {
-    // This reference will give us direct access to the mesh
-    const mesh = useRef()
-    // Set up state for the hovered and active state
-    const [hovered, setHover] = useState(false)
-    const [active, setActive] = useState(false)
-    // Rotate mesh every frame, this is outside of React without overhead
-    useFrame(() => (mesh.current.rotation.x += 0.01))
-    
+const Box = () => {
+
+    const colorMap = useLoader(TextureLoader, Ice)
+
     return (
-        <mesh
-            {...props}
-            ref={mesh}
-            scale={active ? 1.5 : 1}
-            onClick={(event) => setActive(!active)}
-            onPointerOver={(event) => setHover(true)}
-            onPointerOut={(event) => setHover(false)}>
-            <boxGeometry args={[1, 2, 3]} />
-            <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
-
+        <mesh rotation={[90, 0, 20]}>
+            <boxBufferGeometry attach="geometry" args={[3,3,3]}/>
+            {/* <meshLambertMaterial attach="material" color="blue"/> */}
+            <meshStandardMaterial map={colorMap} />
         </mesh>
     )
 }
